48c56b657d9df9a05799edf2d5823d4e0dc938a0
javiergonzalezmontesinos <javier.gonzalez.montesinos@estudiantat.upc.edu>
2025-03-16 14:10:52 +0100

    Añadido envío de datos 'ON' y 'OFF' por el puerto serie (Apartado 2)

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,18 @@
 #include <Arduino.h>
 
 #define LED_BUILTIN 2  // Pin del LED integrado en la ESP32
-#define DELAY 500      // Retardo de 500 ms
+#define DELAY 1000     // Retardo de 1000 ms (1 segundo)
 
 void setup() {
     pinMode(LED_BUILTIN, OUTPUT);  // Configurar el pin como salida
+    Serial.begin(115200);          // Inicializar el puerto serie a 115200 baudios
 }
 
 void loop() {
     digitalWrite(LED_BUILTIN, HIGH);  // Encender el LED
-    delay(DELAY);                      // Esperar 500 ms
-    digitalWrite(LED_BUILTIN, LOW);    // Apagar el LED
-    delay(DELAY);                      // Esperar 500 ms
+    Serial.println("ON");             // Enviar "ON" al puerto serie
+    delay(DELAY);                     // Esperar 1000 ms
+    digitalWrite(LED_BUILTIN, LOW);   // Apagar el LED
+    Serial.println("OFF");            // Enviar "OFF" al puerto serie
+    delay(DELAY);                     // Esperar 1000 ms
 }
